cd43c0f200e5f63d8c7d4c5598824cb7bbdab8ad
Hoon94 <dleognsdl1@naver.com>
2025-01-01 18:00:39 +0900

    feat: ViewModel 뷰 데이터 구현

--- MyCleanProject/MyCleanProject/Presentation/ViewModel/UserListViewModel.swift
+++ MyCleanProject/MyCleanProject/Presentation/ViewModel/UserListViewModel.swift
@@ -27,6 +27,7 @@ public final class UserListViewModel: UserListViewModelProtocol {
     private let fetchUserList = BehaviorRelay<[UserListItem]>(value: [])
     private let allFavoriteUserList = BehaviorRelay<[UserListItem]>(value: [])
     private let favoriteUserList = BehaviorRelay<[UserListItem]>(value: [])
+    private var page = 1
     
     // MARK: - Input & Output
     
@@ -53,13 +54,14 @@ public final class UserListViewModel: UserListViewModelProtocol {
     
     public func transform(input: Input) -> Output {
         input.query.bind { [weak self] query in
-            guard let isValidate = self?.validateQuery(query: query), isValidate else {
+            guard let self = self, validateQuery(query: query) else {
                 self?.getFavoriteUsers(query: "")
                 return
             }
             
-            self?.fetchUser(query: query, page: 0)
-            self?.getFavoriteUsers(query: query)
+            page = 1
+            fetchUser(query: query, page: page)
+            getFavoriteUsers(query: query)
         }.disposed(by: disposeBag)
         
         input.saveFavorite
@@ -67,24 +69,53 @@ public final class UserListViewModel: UserListViewModelProtocol {
                 return (users, query)
             })
             .bind { [weak self] user, query in
-            self?.saveFavoriteUser(user: user, query: query)
-        }.disposed(by: disposeBag)
+                self?.saveFavoriteUser(user: user, query: query)
+            }.disposed(by: disposeBag)
         
         input.deleteFavorite
             .withLatestFrom(input.query, resultSelector: { ($0, $1) })
             .bind { [weak self] userId, query in
                 self?.deleteFavoriteUser(userId: userId, query: query)
-        }.disposed(by: disposeBag)
+            }.disposed(by: disposeBag)
         
-        input.fetchMore.bind {
-            // TODO: 다음 페이지 검색
-        }.disposed(by: disposeBag)
+        input.fetchMore
+            .withLatestFrom(input.query)
+            .bind { [weak self] query in
+                guard let self = self else { return }
+                
+                page += 1
+                fetchUser(query: query, page: page)
+            }.disposed(by: disposeBag)
         
-        let cellData: Observable<[UserListCellData]> = Observable.combineLatest(input.tabButtonType, fetchUserList, favoriteUserList).map { tabButtonType, fetchUserList, favoriteUserList in
-            let cellData: [UserListCellData] = []
-            // TODO: cellData 생성
-            return cellData
-        }
+        let cellData: Observable<[UserListCellData]> = Observable.combineLatest(input.tabButtonType, fetchUserList, favoriteUserList, allFavoriteUserList)
+            .map { [weak self] tabButtonType, fetchUserList, favoriteUserList, allFavoriteUserList in
+                var cellData: [UserListCellData] = []
+                
+                guard let self = self else { return cellData }
+                
+                switch tabButtonType {
+                case .api:
+                    let tuple = useCase.checkFavoriteState(fetchUsers: fetchUserList, favoriteUsers: allFavoriteUserList)
+                    let userCellList = tuple.map { user, isFavorite in
+                        UserListCellData.user(user: user, isFavorite: isFavorite)
+                    }
+                    
+                    return userCellList
+                case .favorite:
+                    let dictionary = useCase.convertListToDictionary(favoriteUsers: favoriteUserList)
+                    let keys = dictionary.keys.sorted()
+                    
+                    keys.forEach { key in
+                        cellData.append(.header(key))
+                        
+                        if let users = dictionary[key] {
+                            cellData += users.map { UserListCellData.user(user: $0, isFavorite: true) }
+                        }
+                    }
+                }
+                
+                return cellData
+            }
         
         return Output(cellData: cellData, error: error.asObservable())
     }
